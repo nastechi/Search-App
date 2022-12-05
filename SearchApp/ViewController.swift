96b245444703e8fd9ca24a6508f7721b4f0e36b2
nastechi <ad.boriskina@icloud.com>
2022-12-05 13:57:59 +0500

    Initial commit; Add navigationController; connect textField and button

--- SearchApp/ViewController.swift
+++ SearchApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
 
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        if let query = searchTextField.text {
+            searchTextField.resignFirstResponder()
+            print(query)
+        }
+    }
+    
+}
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        print(searchTextField.text ?? "no value")
+        return true
+    }
 }
 
